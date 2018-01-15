303b19b65e78854a9cbd18ad18314f90e0f95f1a
Juanma García <juanma.garcia@isobar.com>
2018-01-15 14:58:43 +0000

    Chapter 2 ended.

--- vuebnb-prototype/app.js
+++ vuebnb-prototype/app.js
@@ -9,6 +9,47 @@ var app = new Vue({
     headerImageStyle: {
       'background-image': 'url(sample/header.jpg)'
     },
-    contracted: true
+    opened: false,
+    modalOpen: false,
+    message: 'Hello world'
+  },
+  methods: {
+    escapeKeyListener: function(e) {
+      if (e.keyCode === 27 && this.modalOpen) {
+        this.modalOpen = false;
+      }
+    }
+  },
+  watch: {
+    message: function(newVal, oldVal) {
+      console.log(oldVal, ', ', newVal);
+    },
+    modalOpen: function() {
+      var className = 'modal-open';
+      if (this.modalOpen) {
+        document.body.classList.add(className);
+      } else {
+        document.body.classList.remove(className);
+      }
+    }
+  },
+  created: function() {
+    document.addEventListener('keyup', this.escapeKeyListener);
+  },
+  destroyed: function() {
+    document.removeEventListener('keyup', this.escapeKeyListener);
   }
 });
+
+// document.addEventListener('keyup', function(e){
+//   if (e.keyCode === 27 && app.modalOpen) {
+//     app.modalOpen = false;
+//   }
+// });
+
+setTimeout(function() {
+  app.message = 'Goodbye world';
+  // Output: "Hello world, Goodbye world";
+}, 2000);
+
+// Studying by page 60
